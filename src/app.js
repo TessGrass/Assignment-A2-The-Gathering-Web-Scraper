2a36dc3ddcc74e6fdfca2c20532f83c2c3f6687d
Therese Grass <tg222kv@student.lnu.se>
2021-12-27 17:25:22 +0100

    removed console log

--- src/app.js
+++ src/app.js
@@ -9,11 +9,10 @@ import { CompileData } from './CompileData.js'
 try {
   // Parse the command-line (skip the first two arguments).
   const [,, url] = process.argv
-  console.log(url)
+  // console.log(url)
   // Begin to run the actual application.
   const compileData = new CompileData(url)
   await compileData.run()
-
 } catch (error) {
   console.error(error.message)
 }

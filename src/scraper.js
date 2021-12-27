3cdd4d9b7d3e82f167542773845783c22f65b09f
Therese Grass <tg222kv@student.lnu.se>
2021-12-27 17:24:58 +0100

    removed console logs and general cleanup

--- src/scraper.js
+++ src/scraper.js
@@ -1,38 +1,40 @@
 /**
- * The application module.
+ * The Scraper module.
  *
  * @author Therese Grass <tg222kv@student.lnu.se>
  * @version 1.1.0
  */
 
- import { JSDOM } from 'jsdom'
- import fetch from 'node-fetch'
+import { JSDOM } from 'jsdom'
+import fetch from 'node-fetch'
 
 /**
- * Encapsulates a Node application.
+ *
  */
 export default class Scraper {
-  constructor() {
+  /**
+   *
+   */
+  constructor () {
     this.url = ''
   }
 
-  async scraperInit (url) {
+  /**
+   * @param url
+   */
+  async runScraper (url) {
     this.url = url
     const getDomData = await this.retrieveLinks()
-    console.log(getDomData)
     const domData = new JSDOM(getDomData)
 
     return domData
+  }
 
-   /*  const links = Array.from(domData.window.document.querySelectorAll('a')).map(links => links.href)
-    console.log(links)
-    
-    return [...new Set(links)]
-  }*/
-  
-}
-async retrieveLinks () {
-  const fetchedData = await fetch(this.url)
-  return fetchedData.text()
-}
+  /**
+   *
+   */
+  async retrieveLinks () {
+    const fetchedData = await fetch(this.url)
+    return fetchedData.text()
+  }
 }

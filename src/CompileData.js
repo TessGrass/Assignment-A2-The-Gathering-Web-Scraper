b4db39a33b0a15026df7b3dff8753606a52728d4
Therese Grass <tg222kv@student.lnu.se>
2022-01-07 12:24:24 +0100

    change name on scraper

--- src/CompileData.js
+++ src/CompileData.js
@@ -7,7 +7,7 @@
 
 import { JSDOM } from 'jsdom'
 import fetch from 'node-fetch'
-import Scraper from './Scraper.js'
+import Scraper from './scraper.js'
 
 /**
  * Represents a web scraper application.
